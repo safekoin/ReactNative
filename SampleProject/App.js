acd20e5b320af3d116c39ef8bf92efb6fcc8b49e
safekoin <safekoin@gmail.com>
2020-01-27 07:59:27 -0600

    Image and Submit Updated

--- SampleProject/App.js
+++ SampleProject/App.js
@@ -1,16 +1,96 @@
-import react from "react"
-import { AppRegistry, View, Text } from 'react-native'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-class App extends React.Component {
-  render(){
-    return (
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  TextInput,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
       <View>
-      <Text>Sierra</Text>
-      <Text>Tanner</Text>
-      <Text>Travis</Text>
+      <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+        />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Enter Firstname"
+        />
+      <TextInput
+          style={{height: 40}}
+          placeholder="Enter Your Lastname"
+        />
+        <Button
+        title="Submit"
+        />
       </View>
-      )
-  }
-}
+      </SafeAreaView>
+    </>
+  );
+};
 
-AppRegistry.registerComponent('SampleProject',()=> App)
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
